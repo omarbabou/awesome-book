3aed28dd034e5bc1c4844fc8fc720d2e5d77feda
omarbabou <obabou50@gmail.com>
2022-06-22 23:00:29 +0200

    Add interactivity with js

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,22 @@ const authName = document.getElementById('author');
 
 let bookArray = [];
 let id = 0;
+
+// oject constructor for contact
+
+function Book(id, bookName, authName) {
+    this.id = id;
+    this.bookName = bookName;
+    this.authName = authName;
+  }
+
+
+  // Find the last ID
+
+function lastID(bookArray) {
+    if (bookArray.length > 0) {
+      id = bookArray[bookArray.length - 1].id;
+    } else {
+      id = 0;
+    }
+  }
