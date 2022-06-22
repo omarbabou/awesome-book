74dc4c9dfb5d8ce242b7d419e39961ef481a20e9
omarbabou <obabou50@gmail.com>
2022-06-22 22:50:59 +0200

    Install hint dependencies in json file

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const booksContent = document.querySelector('.displayBook');
+const btn = document.getElementById('button');
+const bookTitle = document.getElementById('name');
+const authName = document.getElementById('author');
+
+let bookArray = [];
+let id = 0;
