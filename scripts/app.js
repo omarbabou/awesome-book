1c7894a25c6624924005a7a537f52430daa9e9c4
omarbabou <obabou50@gmail.com>
2022-06-23 17:27:11 +0200

    Add newbook and removebook in library class

--- scripts/app.js
+++ scripts/app.js
@@ -2,3 +2,14 @@ class Library {
   constructor() {
     this.bookArray = localStorage.getItem('bookstore') ? JSON.parse(localStorage.getItem('bookstore')) : [];
   }
+
+  addNewBook(book) {
+    this.bookArray.push(book);
+    localStorage.setItem('bookstore', JSON.stringify(this.bookArray));
+  }
+
+  removeBook(id) {
+    this.bookArray = this.bookArray.filter((book) => book.id !== Number(id));
+    localStorage.setItem('bookstore', JSON.stringify(this.bookArray));
+  }
+}
