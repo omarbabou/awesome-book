50ada91ea8d079a93786f4f775ce07eff31bf846
omarbabou <obabou50@gmail.com>
2022-06-23 23:28:19 +0200

    add interactivity in js file

--- scripts/main.js
+++ scripts/main.js
@@ -7,18 +7,18 @@ const displayWindow = () => {
 
 navLink.forEach((link) => {
   link.addEventListener('click', () => {
-    if (link.classList.contains('add-contact')) {
+    if (link.classList.contains('add')) {
       displayWindow(link);
-      const newContact = document.querySelector('.contact-form');
+      const newContact = document.querySelector('.form');
       home.style.display = 'none';
       newContact.classList.add('active');
       document.querySelector('h2').textContent = 'Add new Contact';
     } else if (link.classList.contains('contact-me')) {
       displayWindow(link);
-      const aboutMe = document.querySelector('.abt-me');
+      const aboutMe = document.querySelector('.about-me');
       home.style.display = 'none';
       aboutMe.classList.add('active');
-      document.querySelector('h2').textContent = 'Contact me';
+      document.querySelector('h2').textContent = 'Contact';
     } else {
       displayWindow(link);
       home.style.display = 'block';
