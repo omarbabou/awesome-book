9271919bb7931f1c12208241890d0ccb2caf82e6
omarbabou <obabou50@gmail.com>
2022-06-23 17:34:03 +0200

    Declare displayBook as a function

--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,23 @@ class Library {
   }
 }
 
-const booksContainer = document.querySelector('.displayBook');
+const booksContent = document.querySelector('.displayBook');
 const btn = document.getElementById('button');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const bookList = new Library();
 const newBooks = bookList.bookArray;
+
+function displayBook(book) {
+  const newAddDiv = document.createElement('div');
+  const booksContainer = document.querySelector('.displayBook');
+  newAddDiv.classList.add('bookItem');
+  newAddDiv.id = book.id;
+  newAddDiv.innerHTML = `<ul class="book-content">   
+                              <li class = "title">${book.title}</li>
+                              <li>${book.author}</li>
+                          </ul>
+                          <button id="remove">Remove</button>
+  `;
+  booksContainer.appendChild(newAddDiv);
+}
